244d081c0dc6c79bfd901163850c896f24f40a49
PabloSz <pablosaez1995@gmail.com>
2019-09-09 14:43:20 -0300

    update libraries and _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,13 @@
-import App, { Container } from "next/app";
-import Head from "next/head";
+import App from "next/app";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
     return (
-      <Container>
-        <Head>
-          <link
-            rel="shortcut icon"
-            type="image/x-icon"
-            href="/static/favicon.ico"
-          />
-        </Head>
+      <>
         <Component {...pageProps} />
-      </Container>
+      </>
     );
   }
 }
